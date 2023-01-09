9125e82fcf6f3bf9a15d6102540f89538902c3ed
pujara.brijesh1@gmail.com <pujara.brijesh1@gmail.com>
2023-01-09 14:43:57 +0000

    refactor: seperation of tweets into dashboard controller

---
+++ spec/requests/dashboard_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe 'Dashboard', type: :request do
+  describe 'GET /dashboard' do
+    context 'when not signed in' do
+      it "is successful" do 
+        get dashboard_path
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+
+    
+  end
+  
+end
