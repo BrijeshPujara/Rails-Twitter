fc79f73f70d6851c1229f80ba342dbd5c5046531
pujara.brijesh1@gmail.com <pujara.brijesh1@gmail.com>
2023-01-09 14:52:40 +0000

    chore:updated rails_helper to include factorybot syntax and intergration helpers

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -2,14 +2,24 @@ require "rails_helper"
 
 RSpec.describe 'Dashboard', type: :request do
   describe 'GET /dashboard' do
-    context 'when not signed in' do
+    context 'when signed in' do
       it "is successful" do 
         get dashboard_path
         expect(response).to have_http_status(:redirect)
       end
     end
 
-    
+    context 'when not signed in' do
+      it "is not successful" do 
+        user = create(:user)
+        sign_in user
+        get dashboard_path
+        expect(response).to have_http_status(:success)
+
+      end
+    end
+
+
   end
   
 end
